456d2c0513acb4ab33a46da4b02143e860c11b75
AmbarishMangrulkarTR <83744786+AmbarishMangrulkarTR@users.noreply.github.com>
2021-12-10 16:48:14 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,4 +4,4 @@ def lambda_handler(event, context):
     #d = {'col1': [1,2], 'col2': [3,4]}
     #df = pd.DataFrame(data=d)
     #print(df)
-    print('Hello from Source 2')
+    print('Hello from Source 2!')
